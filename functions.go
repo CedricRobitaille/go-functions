e41e7d59d539587dd744f56fe5a8c33d2c3bcfd5
Cedric <crubbertire@rogers.com>
2026-04-14 17:41:41 -0700

    Unit tests

--- functions.go
+++ functions.go
@@ -2,14 +2,28 @@ package main
 
 import "fmt"
 
-func concat(s1 string, s2 string) string {
+func concat(s1, s2 string) string {
 	return s1 + s2
 }
 
 func main() {
-	concatText()
+	// concatText()
 }
 
 func concatText() {
 	fmt.Println(concat("Hello", " world"))
+}
+
+// Paired with the unit test for monthly prices
+func getMonthlyPrice(tier string) int {
+	switch tier {
+	case "basic":
+		return 10000
+	case "premium":
+		return 15000
+	case "enterprise":
+		return 50000
+	default:
+		return 0
+	}
 }
