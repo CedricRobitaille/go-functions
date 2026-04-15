e725f4d3a3bbd7ba9512eb6a33620d1b1701deec
Cedric <crubbertire@rogers.com>
2026-04-14 17:25:32 -0700

    Concat text

--- functions.go
+++ functions.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func concat(s1 string, s2 string) string {
+	return s1 + s2
+}
+
 func main() {
-	fmt.Println("Hello world")
+	concatText()
+}
+
+func concatText() {
+	fmt.Println(concat("Hello", " world"))
 }
