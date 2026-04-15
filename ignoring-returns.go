2fa787ea83b3b37d4a1bf3adb29e14cedccf9014
Cedric <crubbertire@rogers.com>
2026-04-15 09:20:19 -0700

    ignoring return values

---
+++ ignoring-returns.go
@@ -0,0 +1,20 @@
+package main
+
+func getProductMessage(tier string) string {
+	// You can ignore a return value using the `_`
+	// This is useful if you have circle, and only need it's center point
+	quantityMsg, priceMsg, _ := getProductInfo(tier)
+	return "You get " + quantityMsg + " for " + priceMsg + "."
+}
+
+func getProductInfo(tier string) (string, string, string) {
+	if tier == "basic" {
+		return "1,000 texts per month", "$30 per month", "most popular"
+	} else if tier == "premium" {
+		return "50,000 texts per month", "$60 per month", "best value"
+	} else if tier == "enterprise" {
+		return "unlimited texts per month", "$100 per month", "customizable"
+	} else {
+		return "", "", ""
+	}
+}
